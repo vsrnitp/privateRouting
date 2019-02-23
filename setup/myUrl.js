764f05a307e614556af094cf1f87030e647e8733
Vishwajeet Kumar Singh <35272198+vsrnitp@users.noreply.github.com>
2019-02-23 11:07:30 +0530

    Create myUrl.js

---
+++ setup/myUrl.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURL:"mongodb://USERNAME:PAASSWORD@ds155823.mlab.com:55823/worshiapp",
+    secret:"mystrongsecret"
+};
+// username and password intentionally not provided
