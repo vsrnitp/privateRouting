994ab52afd572cae334450e804ee888ba2121f20
Vishwajeet Kumar Singh <35272198+vsrnitp@users.noreply.github.com>
2019-02-23 11:13:04 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const auth = require('./routes/api/auth');
+const app = express();
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+app.use(express.static('public'));
+app.use(express.static(__dirname + '/resources'));
+app.set('view engine','ejs');
+
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+
+//passport midlleware
+app.use(passport.initialize());
+
+// config for jwt strategy
+require('./strategies/jsonwtStrategy')(passport)
+
+
+// Connect to MongoDB
+mongoose.connect('mongodb://vsr:Hello12345@ds155823.mlab.com:55823/worshiapp', {useNewUrlParser: true});
+mongoose.connection.once('open', function(){
+  console.log('Conection has been made!');
+}).on('error', function(error){
+    console.log('Error is: ', error);
+});
+
+
+
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+app.use('/api/auth',auth);
+
+app.listen(8080,console.log('App working fine'));
